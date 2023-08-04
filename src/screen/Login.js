1d0e527618279649cc10f77b2f7f3d3e0eff82f4
AnujSingh25 <anuj.singh020799@gmail.com>
2023-08-04 13:12:19 +0530

    firebase push notifications

--- src/screen/Login.js
+++ src/screen/Login.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from "react";
 import { useNavigation } from "@react-navigation/native";
-import { SafeAreaView, Text, View, StyleSheet, Pressable, Alert, KeyboardAvoidingView, Image } from "react-native";
+import { SafeAreaView, Text, View, StyleSheet, Pressable, Alert, KeyboardAvoidingView, Image, Dimensions } from "react-native";
 import { TextInput } from "react-native-paper";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useFocusEffect, useIsFocused } from "@react-navigation/native";
+import messaging from '@react-native-firebase/messaging'
 
 const Login = (props) => {
 
@@ -36,7 +37,21 @@ const Login = (props) => {
         if (onBack == true) {
             clearData()
         }
+        token()
     }, [onBack])
+
+    const token = async () => {
+        const token = await messaging().getToken();
+        console.log(token);
+    }
+
+    useEffect(() => {
+        const unsubscribe = messaging().onMessage(async remoteMessage => {
+            Alert.alert('A new FCM message arrived!', JSON.stringify(remoteMessage));
+        });
+
+        return unsubscribe;
+    }, []);
 
     const checkTextInput = async () => {
 
@@ -169,3 +184,31 @@ const style = StyleSheet.create({
     }
 })
 export default Login
+
+
+// Variant: debug
+// Config: debug
+// Store: D:\React projects\Reactnative\android\app\debug.keystore
+// Alias: androiddebugkey
+// MD5: 20:F4:61:48:B7:2D:8E:5E:5C:A2:3D:37:A4:F4:14:90
+// SHA1: 5E:8F:16:06:2E:A3:CD:2C:4A:0D:54:78:76:BA:A6:F3:8C:AB:F6:25
+// SHA-256: FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C
+// Valid until: Wednesday, May 1, 2052
+// ----------
+// Variant: release
+// Config: debug
+// Store: D:\React projects\Reactnative\android\app\debug.keystore
+// Alias: androiddebugkey
+// MD5: 20:F4:61:48:B7:2D:8E:5E:5C:A2:3D:37:A4:F4:14:90
+// SHA1: 5E:8F:16:06:2E:A3:CD:2C:4A:0D:54:78:76:BA:A6:F3:8C:AB:F6:25
+// SHA-256: FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C
+// Valid until: Wednesday, May 1, 2052
+// ----------
+// Variant: debugAndroidTest
+// Config: debug
+// Store: D:\React projects\Reactnative\android\app\debug.keystore
+// Alias: androiddebugkey
+// MD5: 20:F4:61:48:B7:2D:8E:5E:5C:A2:3D:37:A4:F4:14:90
+// SHA1: 5E:8F:16:06:2E:A3:CD:2C:4A:0D:54:78:76:BA:A6:F3:8C:AB:F6:25
+// SHA-256: FA:C6:17:45:DC:09:03:78:6F:B9:ED:E6:2A:96:2B:39:9F:73:48:F0:BB:6F:89:9B:83:32:66:75:91:03:3B:9C
+// Valid until: Wednesday, May 1, 2052
